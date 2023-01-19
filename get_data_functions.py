9830961697ff45839af64e3e1f428c926e93b514
Michael R. Gallagher <michael.r.gallagher@noaa.gov>
2023-01-18 19:43:50 -0700

    update get_data to support xarray for easier level3 processing

--- get_data_functions.py
+++ get_data_functions.py
@@ -11,7 +11,7 @@ from multiprocessing import Queue   as Q
 
 def get_flux_data(station, start_day, end_day, level,
                   data_dir='/Projects/MOSAiC/', data_type='slow',
-                  verbose=False, nthreads=1, pickle_dir=None):
+                  verbose=False, nthreads=1, as_xrds=False, pickle_dir=None):
 
 
     """ Get a dataset from the MOSAiC flux project. 
@@ -53,19 +53,22 @@ def get_flux_data(station, start_day, end_day, level,
         print("\n\nYou asked for a station name that doesn't exist...")
         print("... can't help you here\n\n"); raise IOError
 
-    pickled_filename = f'{station}_{level}_{data_type}_df' # code_version is tacked on to this when run
+    if as_xrds: data_format = 'ds'
+    else: data_format = 'df'
+
+    pickled_filename = f'{station}_{level}_{data_type}_{data_format}' # code_version is tacked on to this when run
     df = pd.DataFrame() # dataframe and version we return from this function
     code_version = "?"
     was_pickled = False
     if pickle_dir:
-        print(f"\n!!! searching for pickle file containing {pickled_filename} and loading it, takes time...\n")
+        if verbose: print(f"\n!!! searching for pickle file containing {pickled_filename} and loading it, takes time...\n")
         for root, dirs, files in os.walk(pickle_dir): # pretty stupid way to find the file
             for filename in files:
                 if pickled_filename in filename:
                     filename = pickle_dir+filename
 
                     with open(filename, "rb") as f:
-                        df = pickle.load(f)
+                        data_obj = pickle.load(f)
                     name_words = filename.rpartition('_')[-1].rpartition('.')
                     code_version = f"{name_words[0]}.{name_words[1]}"
                     was_pickled = True
@@ -75,7 +78,7 @@ def get_flux_data(station, start_day, end_day, level,
         if not was_pickled: print("... didn't find a pickle, we'll write one !!!\n\n")
 
     if not was_pickled: 
-        df_list = [] # data frames get appended here in loop and then concatted by function after
+        data_list = [] # data frames get appended here in loop and then concatted by function after
         day_series = pd.date_range(start_day, end_day) 
 
         q_list = []; p_list = []; day_list = []
@@ -98,7 +101,7 @@ def get_flux_data(station, start_day, end_day, level,
                 file_str = f'/mosflx{station}{data_type}.level{level}.{date_str}.nc'
                 if level == 2:
                     #subdir = subdir+'version3/'
-                    subdir = subdir+'finalqc/'
+                    #subdir = subdir+'finalqc/'
                     cadence = '1'
                     if data_type=='seb': cadence = '10'
                     #file_str = f'/mos{data_type}.metcity.level{level}v3.{cadence}min.{date_str}.nc'
@@ -110,7 +113,6 @@ def get_flux_data(station, start_day, end_day, level,
                     cadence = '1'
                     if data_type=='seb': cadence = '10'
                     file_str = f'/mos{data_type}.{station}.level{level}.4.{cadence}min.{date_str}.nc'
-                    subdir = subdir+'finalqc/'
                     #subdir = subdir+'/version3'
 
 
@@ -118,38 +120,53 @@ def get_flux_data(station, start_day, end_day, level,
             curr_file = files_dir+file_str
 
             q_today = Q()
-            P(target=get_datafile, args=(curr_file, q_today),).start()
+            P(target=get_datafile, args=(curr_file, as_xrds , q_today),).start()
             q_list.append(q_today)
             day_list.append(today)
             if (i_day+1) % nthreads == 0 or today == day_series[-1]:
                 for qq in q_list:
-                    df_today = qq.get()
+                    data_today = qq.get()
                     cv = qq.get()
                     if cv!=None: code_version = cv # assume all files have same code version, save only one
-                    if not df_today.empty: 
-                        df_list.append(df_today.copy())
+                    if type(data_today) in [type(pd.DataFrame()), type(xr.Dataset())]:
+                        data_list.append(data_today.copy())
                 q_list = []
                 day_list = []
 
         if verbose: print("... concatting, takes some time...")
-        try    : df = pd.concat(df_list)
-        except : pd.DataFrame()
+        if as_xrds: 
 
-        try: 
-            df.index = df.index.droplevel("freq")
-            df       = df[~df.index.duplicated(keep='first')]
-        except: pass # this only applices to 'seb'
+            try    :
+                if verbose: print(data_list[0])
+                ds = xr.concat(data_list, 'time')
+            except Exception as e :
+                if verbose: print("-----------------------")
+                if verbose: print(e)
+                ds = xr.Dataset()   
 
 
-        time_dates = df.index
-        df['time'] = time_dates # duplicates index... but it can be convenient
+            data_obj = ds
+
+        else:
+            try    : df = pd.concat(data_list)
+            except : pd.DataFrame()
+
+            try: 
+                df.index = df.index.droplevel("freq")
+                df       = df[~df.index.duplicated(keep='first')]
+            except: pass # this only applices to 'seb', no need to worry. more specific exception though? 
+
+            time_dates = df.index
+            df['time'] = time_dates # duplicates index... but it can be convenient
+
+            data_obj = df
+
 
         if verbose:
             print('\n ... data sample :')
             print('================')
-            print(df)
+            print(data_obj)
             print('\n')
-            print(df.info())
             print('================\n\n') 
 
         if pickle_dir:
@@ -158,34 +175,64 @@ def get_flux_data(station, start_day, end_day, level,
             print("... copy this manually to a ramdisk somewhere for bonus speed points")
             print("... must be symlinked here to be seen by this routine")
             print("...\n... this takes a minute, patience\n\n")
-            df.to_pickle(f"{pickle_dir}/{pickled_filename}_{code_version[0:3]}.pkl")
+            pickle_name = f"{pickle_dir}/{pickled_filename}_{code_version[0:3]}.pkl"
 
-    return df, code_version 
+            with open(pickle_name, 'wb') as pf:
+                retcode = pickle.dump(data_obj, pf)#, protocol=pickle.HIGHEST_PROTOCOL)
+ 
+    return data_obj, code_version 
 
-def get_datafile(curr_file, q=None):
+def get_datafile(curr_file, as_xrds=False, q=None):
 
     if os.path.isfile(curr_file):
+        print(f'... got {curr_file}')
         try: 
-            with xr.load_dataset(curr_file, engine='netcdf4') as xarr_ds: data_frame = xarr_ds.to_dataframe()
+            with xr.load_dataset(curr_file, engine='netcdf4') as xarr_ds:
+                curr_ds = xarr_ds
+                data_frame = xarr_ds.to_dataframe()
         except Exception as e: 
-            print(curr_file, e, 'wtf')
-            return None 
+            print(curr_file, e, 'wtf, this should absolutely never happen')
+            try:
+                q.put(xr.Dataset())
 
+            except Exception as e:
+                return xr.Dataset()
+
+            try:
+                q.put('unknown'); return 'unknown'
+            except:
+                return 'unknown'
 
     else:
         print(f"!!! requested file doesn't exist : {curr_file}")
-        data_frame   = pd.DataFrame()
+        curr_ds = None
+        data_frame = None
 
-    try:    q.put(data_frame)
-    except: return data_frame
+    if as_xrds: 
 
-    try:
-        code_version = xarr_ds.attrs['version']
-        q.put(code_version)
-        return
-    except:
-        q.put("unknown") # code version threw exception
-        return data_frame # can be implicit but doesn't matter, really
+        try:    q.put(curr_ds)
+        except: return curr_ds
+
+        try:
+            code_version = curr_ds.attrs['version']
+            q.put(code_version)
+            return
+        except:
+            q.put("unknown") # code version threw exception
+            return curr_ds   # can be implicit but doesn't matter, really
+        
+    else:
+        try:    q.put(data_frame)
+        except: return data_frame
+
+        try:
+            code_version = curr_ds.attrs['version']
+            q.put(code_version)
+            return
+        except:
+            q.put("unknown") # code version threw exception
+            return data_frame # can be implicit but doesn't matter, really
+
 
 def get_ship_df(ship_data_dir='/Projects/MOSAiC_internal/partner_data/AWI/polarstern/WXstation/'):
 
@@ -282,7 +329,7 @@ def get_arm_radiation_data(start_day, end_day, data_dir='/Projects/MOSAiC/',
             if verbose: 
                 print(f"  ... {curr_file}")
             q_today = Q()
-            P(target=get_datafile, args=(curr_file, q_today),).start()
+            P(target=get_datafile, args=(curr_file, False, q_today),).start()
             q_list.append(q_today)
             day_list.append(today)
             if (i_day+1) % nthreads == 0 or today == day_series[-1]:
@@ -290,7 +337,7 @@ def get_arm_radiation_data(start_day, end_day, data_dir='/Projects/MOSAiC/',
                     df_today = qq.get()
                     cv = qq.get()
                     if cv!=None: code_version = cv # assume all files have same code version, save only one
-                    if not df_today.empty: 
+                    if type(df_today) == type(pd.DataFrame()): 
                         df_list.append(df_today.copy())
                 q_list = []
                 day_list = []
